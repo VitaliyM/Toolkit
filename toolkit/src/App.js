dc7138d9928b0c2e32232fd02dcee7753a1069a4
VitaliyM <vetalmamchur@gmail.com>
2023-02-22 15:16:36 +0000

    2

--- toolkit/src/App.js
+++ toolkit/src/App.js
@@ -5,6 +5,21 @@ function App() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState('');
 
+  const toggleTodoComplete = (todoId) => {
+    setTodos(
+      todos.map(
+        todo => {
+          if (todo.id !== todoId) return todo;
+
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+      )
+    )
+  }
+
   const addTodo = () => {
     if (text.trim().length) {        //Убрать все пробелы, проверить есть ли длина
       setTodos([
@@ -19,20 +34,25 @@ function App() {
     }
   }
 
+  const removeTodo = (todoId) => {
+    setTodos(todos.filter(todo => todo.id !== todoId)) //Метод filter возвращает все элементы соответсвующие условию
+  }
+
   return (
     <div className="App">
       <label>
-        <input className='input-place' value={text} onChange={(e) => setText(e.target.value)} />
+        <input className='input-place' value={text} onChange={ (e) => setText(e.target.value) } />
         <button onClick={addTodo}>Add Todo</button>
       </label>
 
       <ul>
         {
-          todos.map(todo => <li key={todo.id}>
-            <input type="checkbox" />
-            <span>{todo.text}</span>
-            <span className='delete'>&times;</span>
-          </li>)
+          todos.map(todo =>
+            <li key={todo.id}>
+              <input type="checkbox" checked={todo.completed} onChange={() => toggleTodoComplete(todo.id)} />
+              <span>{todo.text}</span>
+              <span className='delete' onClick={() => removeTodo(todo.id)}>&times;</span>
+            </li>)
         }
 
       </ul>
